2bcbf8d5e3166c92a18bf492493ea934ab66c281
Dimas Rizky <dimasrizkym3@gmail.com>
2025-07-04 13:57:28 +0700

    delete routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,5 @@
-import { Routes, Route } from "react-router-dom";
-import Login from "./pages/Auth/Login";
-import Register from "./pages/Auth/Register";
-import Home from "./pages/Dashboard/Home";
-import CreateKantong from "./pages/Kantong/CreateKantong";
-import DetailKantong from "./pages/Kantong/DetailKantong";
-import EditKantong from "./pages/Kantong/EditKantong";
-import KirimUang from "./pages/Transaction/KirimUang";
-import PindahUang from "./pages/Transaction/PindahUang";
-import RiwayatTransaksi from "./pages/Transaction/RiwayatTransaksi";
-
 function App() {
-  return (
-    <div className="bg-gray-50 min-h-screen">
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/kantong/new" element={<CreateKantong />} />
-        <Route path="/kantong/:id" element={<DetailKantong />} />
-        <Route path="/kantong/:id/edit" element={<EditKantong />} />
-        <Route path="/transfer/send" element={<KirimUang />} />
-        <Route path="/transfer/move" element={<PindahUang />} />
-        <Route path="/history" element={<RiwayatTransaksi />} />
-      </Routes>
-    </div>
-  );
+  return <h1>hello</h1>;
 }
 
 export default App;
